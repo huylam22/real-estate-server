9ae68a643e91a5d9fb5eccbfff3f9f00b78d3502
Huy Lam <huy.lam0199@gmail.com>
2023-03-21 15:00:46 +0700

    delete unused model

--- src/main/java/com/devcamp/realestatebackend/controllers/PropertyPhotoController.java
+++ src/main/java/com/devcamp/realestatebackend/controllers/PropertyPhotoController.java
@@ -13,7 +13,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
-import com.devcamp.realestatebackend.repositories.PropertyPhotoRepository;
 import com.devcamp.realestatebackend.services.PropertyPhotoService;
 
 @RestController
